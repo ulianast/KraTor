684baed03f6eda08dc67857eb255347b7dfa2f21
ulianast <uliana.krasnova@gmail.com>
2013-11-02 18:02:43 +0300

    Update Peer.hpp

--- Peer.hpp
+++ Peer.hpp
@@ -5,27 +5,40 @@ class Peer
 public:
 	//Identificators
 	string peer_id;	
-	short port;
-	int IP_address;
+	unsigned short port;
+	string IP_address;
+	SOCKET sSocket;
 	int key;                   //A unique key that is randomized by the client
 
 	// Info Payload
 	char info_hash[20];	
 	long long int uploaded;
 	long long int downloaded;
-	long long int left;			
+	long long int left;	
+	char * bitfield;
 	string pieces;
+	long long int piece_length;	
+	
+	// Flags
+	bool bSentHandShake;
+	bool bSentBitfield;
+	bool bChokedByMe;
+	bool bChokingMe;
+	bool bInterestedInMe;
+	bool bInteresting;
 	
 
-	enum event
+	/*enum event
 	{
 		none,
 		completed,
 		started,
 		stopped
-	};
+	};*/
 
-	void Initialise(struct MetaInfo meta_info);
+	//Peer();
+	Peer(struct MetaInfo meta_info);
+	Peer();
 	//Peer(struct MetaInfo meta_info);	
 	
 };
