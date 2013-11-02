3a9ed8171a923722dbde8252bb1407aa72aba7ba
ulianast <uliana.krasnova@gmail.com>
2013-11-02 18:03:41 +0300

    Create stdafx.h

---
+++ stdafx.h
@@ -0,0 +1,35 @@
+// stdafx.h: включаемый файл для стандартных системных включаемых файлов
+// или включаемых файлов для конкретного проекта, которые часто используются, но
+// не часто изменяются
+//
+
+#pragma once
+
+#define _SCL_SECURE_NO_WARNINGS
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+
+#include <string>
+#include <Windows.h>
+#include <stdlib.h>
+#include <list>
+#include <time.h>
+#include <string.h>
+#include <map>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <locale>
+
+//#include <boost/filesystem.hpp>
+//#include <boost/thread.hpp>
+
+//#include <WinSock2.h>
+using namespace std;
+
+
+
+// TODO: Установите здесь ссылки на дополнительные заголовки, требующиеся для программы
