358472eca1049b0b4da4beee4dfd5d26ba81a375
ulianast <uliana.krasnova@gmail.com>
2013-11-02 18:01:08 +0300

    Update Peer.cpp

--- Peer.cpp
+++ Peer.cpp
@@ -3,19 +3,26 @@
 #include "MetaInfo.h"
 
 
-void Peer::Initialise(struct MetaInfo meta_info)
+Peer::Peer(struct MetaInfo meta_info)
 {
 
+	Peer();
 	pieces.assign(meta_info.pieces);
-	uploaded=0;
-	downloaded=0;
-	port=5050;
+
+	//port=5050;
+	
+	// initializing bitfield ( all bits set to 'false' for default)
+	long long int bitfield_size=(meta_info.total_size/meta_info.piece_length+7)/8;
+	bitfield=new char[bitfield_size];
+	for(int i=0;i<bitfield_size;i++) bitfield[i]=0;
+
+	// info_hash
 	for(int i=0;i<20;i++)info_hash[i]=meta_info.info_hash[i];
-	//strncpy_s((char*)(info_hash),20,(const char*)(meta_info.info_hash),20);
 	meta_info.pieces.assign(pieces);
 	left=meta_info.total_size;
 	
-	peer_id="-Kr1000-";	                            //peer_id
+	//setting per_id
+	peer_id="-Kr1000-";	                            
 	static const char alphanum[] =
 		"0123456789"
 		"!@#$%^&*"
@@ -26,5 +33,20 @@ void Peer::Initialise(struct MetaInfo meta_info)
 	for(int i=0;i<12;i++)
 		peer_id+=alphanum[rand() % stringLength];
 
+	// seting key ( random value )
 	key=rand();
+
+	piece_length=meta_info.piece_length;
+}
+
+Peer::Peer()
+{
+	uploaded=0;
+	downloaded=0;
+	bSentHandShake=false;
+	bSentBitfield=false;
+	bInterestedInMe=false;
+	bInteresting=false;
+	bChokedByMe=true;
+	bChokingMe=true;
 }
